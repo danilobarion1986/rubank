a8208f95cd24e81f23dca5bc31ca01af62a72ed3
Danilo Barion Nogueira <danilob@knowbe4.com>
2020-07-21 19:10:59 -0300

    Add specs and implementation for authentication errors

--- lib/rubank/client.rb
+++ lib/rubank/client.rb
@@ -6,8 +6,20 @@ module Rubank
   class Client
     class AuthenticationError < StandardError; end
 
-    def authenticate(_username: nil, _password: nil)
-      raise AuthenticationError, "Credentials are either wrong or missing!"
+    attr_reader :urls
+
+    def initialize
+      @urls = Requesters::UrlsDiscoverer.call
+    end
+
+    def authenticate(username: nil, password: nil)
+      username ||= ENV['NUBANK_USERNAME']
+      password ||= ENV['NUBANK_PASSWORD']
+
+      raise AuthenticationError, "Credentials are missing!" unless username && password
+
+      response = Requesters::CredentialsAuthenticator.call(urls, username, password)
+      raise AuthenticationError, "Credentials are missing!" if response[:error]
     end
   end
 end
--- spec/lib/client_spec.rb
+++ spec/lib/client_spec.rb
@@ -3,10 +3,38 @@
 RSpec.describe Rubank::Client do
   subject { described_class.new }
 
+  before do
+    allow(Rubank::Requesters::UrlsDiscoverer).to receive(:call).and_return({ login: 'url'})
+  end
+
   describe "#authenticate" do
-    context "when no credential are passing" do
+    context "when no credentials are passed" do
       it "raises Rubank::Client::AuthenticationError" do
         expect { subject.authenticate }.to raise_error(Rubank::Client::AuthenticationError)
+      end
+    end
+
+    context "when the credentials are passed" do
+      context "when they are incorrect" do
+        it "raises Rubank::Client::AuthenticationError" do
+          allow(Rubank::Requesters::CredentialsAuthenticator).to(
+            receive(:call).and_return({ error: 'Unauthorized' })
+          )
+
+          expect { subject.authenticate(username: anything, password: anything) }.to(
+            raise_error(Rubank::Client::AuthenticationError)
+          )
+        end
+      end
+
+      context "when they are correct" do
+        it "do not raises error" do
+          allow(Rubank::Requesters::CredentialsAuthenticator).to(
+            receive(:call).and_return({ ok: 'Ok' })
+          )
+
+          expect { subject.authenticate(username: anything, password: anything) }.to_not raise_error
+        end
       end
     end
   end
