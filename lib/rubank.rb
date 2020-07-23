91cfabd98e5c7ac0d1c077e13b5294616c86ff39
Danilo Barion Nogueira <danilob@knowbe4.com>
2020-07-23 10:40:47 -0300

    Add configuration gem

--- lib/rubank.rb
+++ lib/rubank.rb
@@ -2,10 +2,22 @@
 
 require "rubank/version"
 require "rubank/client"
+require "dry/configurable"
 
 # :no-doc:
 module Rubank
+  extend Dry::Configurable
+
   class Error < StandardError; end
+
+  # Pass a block for nested configuration (works to any depth)
+  setting :authentication do
+    # Can pass a default value
+    setting :qrcode do
+      setting :render_format, :html
+      setting :seconds_to_scan, 15
+    end
+  end
 
   def self.version
     Rubank::VERSION
